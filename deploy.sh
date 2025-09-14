e093cf5019950aaae597372cac986a27304d8742
Amal Raj <amalraj.a@palebluesoftware.com>
2025-09-14 20:00:43 +0530

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # Stop and remove old container if running
+set -e
+cd /tmp/devops-build || exit 1
+IMAGE_NAME=$1
+CONTAINER_NAME=$1
+DOCKER_USERNAME=$2
+DOCKER_PASSWORD=$3
+
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
     echo "Stopping old container..."
     docker stop $CONTAINER_NAME
@@ -8,6 +17,6 @@ fi
 
 # Run new container
 echo "Deploying new container..."
-docker run -d --name $CONTAINER_NAME -p 80:80 $IMAGE_NAME:$IMAGE_TAG
+docker run -d --name $CONTAINER_NAME -p 80:80 $IMAGE_NAME:latest
 
 echo "Deployment complete. App is running on port 80."
