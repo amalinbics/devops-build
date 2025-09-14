09ccb3789b318b980cd183f6017af8328cfbdb72
Amal Raj <amalraj.a@palebluesoftware.com>
2025-09-14 19:03:29 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ IMAGE_NAME=$1
 IMAGE_TAG=$2
 echo "Building Docker image..."
 docker build -t $IMAGE_NAME:$IMAGE_TAG .
-docker push ${IMAGE_NAME}:${IMAGE_TAG}
 docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_NAME}:latest
 docker push ${IMAGE_NAME}:latest
 echo "Build complete: $IMAGE_NAME:$IMAGE_TAG"
