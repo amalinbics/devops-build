ac90a727f7ec5e38755d370943835d668db37028
Amal Raj <amalraj.a@palebluesoftware.com>
2025-09-16 17:22:24 +0530

    updated container name in jenkins file

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -114,9 +114,14 @@ resource "aws_security_group" "dev_server_sg" {
     cidr_blocks = [format("%s/32", var.my_ip)]
   }
 
-
-
-
+  ingress {
+    description = "cadvisor access"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   egress {
     description = "Allow all outbound traffic"
     from_port   = 0
@@ -140,10 +145,18 @@ resource "aws_security_group" "prod_server_sg" {
     cidr_blocks = [format("%s/32", var.my_ip)]
   }
 
-   ingress {
+  ingress {
     description = "http access"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "cadvisor access"
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
